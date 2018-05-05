1250f92d3fb4b522a1bbb9d93b26a60edfe291a5
AlexandrVN <alexandr.novohatko@gmail.com>
2018-05-05 03:36:50 +0300

    update Project ORDER: showing users

--- src/main/java/com/order/dao/UserDaoImpl.java
+++ src/main/java/com/order/dao/UserDaoImpl.java
@@ -17,8 +17,32 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
+    public void save(User user) {
+        String sql = "INSERT INTO user (name, email, age) VALUE (?, ?, ?)";
+        jdbcTemplate.update(sql, user.getName(), user.getEmail(), user.getAge());
+    }
+
+    @Override
+    public User getById(int id) {
+        String sql = "SELECT * FROM user WHERE id=?";
+        return jdbcTemplate.queryForObject(sql, new UserMapper(), id);
+    }
+
+    @Override
     public List<User> findAll() {
         String sql = "SELECT * FROM user";
         return jdbcTemplate.query(sql, new UserMapper());
+    }
+
+    @Override
+    public void update(User user) {
+        String sql = "UPDATE user SET name=?, email=?, age=? WHERE id=?";
+        jdbcTemplate.update(sql, user.getName(), user.getEmail(), user.getAge(), user.getId());
+    }
+
+    @Override
+    public void delete(int id) {
+        String sql = "DELETE FROM user WHERE id=?";
+        jdbcTemplate.update(sql, id);
     }
 }
